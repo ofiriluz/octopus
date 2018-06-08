f7ef2599cbda71541e5e45db520050d978249ef6
Isan-Rivkin <isanrivkin@gmail.com>
2018-06-09 01:07:48 +0300

    tweepy get user

--- src/access_points/twitter_ap/twitter_api.py
+++ src/access_points/twitter_ap/twitter_api.py
@@ -1,12 +1,26 @@
 import tweepy
+from src.access_points.auth_keys import TWITTER_ACCESS_TOKEN,TWITTER_TOKEN_SECRET,TWITTER_API_KEY,TWITTER_API_SECRET
 
 
+class TwitterAPI(object):
+    def __init__(self):
+        self.auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+        self.auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET)
+        self.api = tweepy.API(self.auth)
+    # testing => get our account tweets
+    def get_self_tweets(self):
+        tweets = [tweet.text for tweet in self.api.home_timeline()]
+        return tweets
 
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET)
+    # Get the User object for twitter...
+    def get_user(self,user_name):
+        user = self.api.get_user(user_name)
+        return user
 
-api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print (tweet.text)
+if __name__ == "__main__":
+    user = TwitterAPI().get_user("isan_rivkin")
+    print(user.screen_name)
+    print(user.followers_count)
+    for friend in user.friends():
+        print(friend.screen_name)
