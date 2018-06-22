b1da8a909ff288310be2ec01f4a3bc2d839038af
Isan-Rivkin <isanrivkin@gmail.com>
2018-06-22 16:36:55 +0300

    further research of github api

--- octopus/access_points/github_ap/github_api.py
+++ octopus/access_points/github_ap/github_api.py
@@ -15,3 +15,23 @@ if __name__ == "__main__":
     users = g.search_users('isan rivkin')
     for u in users:
         print(u.login)
+        repos = g.get_user(u.login).get_repos()
+        count = 0
+        for repo in repos:
+            count += 1
+            print(repo.name)
+            print(repo.language)
+            # the current repo owner (if its fork then the forker is the owner
+            print(repo.owner.login)
+            # the parent owner if it's forke then it's the the owner original
+            print(repo.owner.parent)
+            # pushed at
+            print(repo.pushed_at.datetime)
+            # watchers count
+            print(repo.watchers_count)
+            # is this repo  a fork ?
+            print(repo.fork)
+            # did anyone fork this repo ?
+            print(repo.forks_count)
+
+        print('repos #{}'.format(count))
