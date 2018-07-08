0bd20ab6455d1cacd776a801cfd1c8a3b2d32566
Ofir Iluz <iluzofir@gmail.com>
2018-07-08 14:41:00 +0300

    Initial working score, need to work fix etc

--- octopus/profiler/ap_profilers/github/github_profiler.py
+++ octopus/profiler/ap_profilers/github/github_profiler.py
@@ -2,6 +2,7 @@ import os
 import json
 import numpy
 import datetime
+import pprint
 from octopus.profiler.ap_profilers.access_point_profiler import AccessPointProfiler
 
 class GitHubProfiler(AccessPointProfiler):
@@ -10,6 +11,26 @@ class GitHubProfiler(AccessPointProfiler):
         self.__user_metadata = None
         self.__user_repos_metadata = []
         self.__user_profiler_results = {}
+        self.__weights = \
+        { 
+            'repo': 
+            {
+                'administirive': 0.2,
+                'branches': 0.6,
+                'fork': 0.2
+            },
+            'branch':
+            {
+                'rolling': 0.3,
+                'importance': 0.3,
+                'commits': 0.4
+            },
+            'commit': 
+            {
+                'sizes': 0.4,
+                'changes': 0.6
+            }
+        }
 
     def __read_user_workspace(self):
         # Assert user workspace path
@@ -43,6 +64,9 @@ class GitHubProfiler(AccessPointProfiler):
         #               ([Count(Issues)] + [Count(PullReqs)])
         administrative_contribution_score = 0.0
         posts_len = len(repo['issues']) + len(repo['pullreqs'])
+        if posts_len == 0:
+            return administrative_contribution_score
+
         for issue in zip(repo['issues'], repo['pullreqs']):
             comments_len = len(issue['comments'])
             user_comments_len = 0
@@ -50,8 +74,9 @@ class GitHubProfiler(AccessPointProfiler):
                 if comment['author'] == self.__user_metadata['user']:
                     user_comments_len = user_comments_len + 1
             administrative_contribution_score = administrative_contribution_score + user_comments_len/comments_len
+
         # Normalize the sum between 0 and 1
-        administrative_contribution_score = (1/posts_len-0)*(administrative_contribution_score-posts_len)+1
+        administrative_contribution_score = (1/posts_len)*(administrative_contribution_score-posts_len)+1
 
         return administrative_contribution_score
 
@@ -70,24 +95,28 @@ class GitHubProfiler(AccessPointProfiler):
 
         rolling_scores = []
         for commit in branch['commits']:
-            changes = commit['changes']
+            # pprint.pprint(commit)
+            changes = commit['file_changes']
             
             # The more changes across files, the better contribution
             changes_perc = 0.0
             for change in changes:
                 # Read file line amount after commit
-                lines = change['line_amount']
-                change_amount = change['change_amount']
+                lines = change['file_line_count']
+                change_amount = change['file_changed_lines_count']
                 actual_changes_len = lines - change_amount
                 change_perc = actual_changes_len / lines
                 changes_perc = changes_perc + change_perc
-            commit_perc_score = changes_perc / commit['post']['file_amount']
+            commit_perc_score = changes_perc / commit['post_commit']['file_amount']
             rolling_scores.append(commit_perc_score)
         # Calculate the average deltas, this will give us the commitments to the branch overtime
         # Can be plotted later on
         drolling_scores = numpy.diff(rolling_scores)
 
         return numpy.average(drolling_scores)
+
+    def __get_master_branch(self, repo):
+        return next((branch for branch in repo['branches'] if branch['name'] == 'master'))
 
     def __calculate_branch_importance(self, repo, branch, master_branch):
         #       Branch importance to master
@@ -103,11 +132,14 @@ class GitHubProfiler(AccessPointProfiler):
         # Sorted recent to oldest
         last_branch_cmmmit = branch['commits'][0]
         last_master_commit = master_branch['commits'][0]
+        last_branch_commit_time = datetime.datetime.strptime(last_branch_cmmmit['commit_time'], '%d.%m.%Y')
+        last_master_commit_time = datetime.datetime.strptime(last_master_commit['commit_time'], '%d.%m.%Y')
+        repo_creation_time = datetime.datetime.strptime(repo['creation_time'], '%d.%m.%Y')
 
-        date_diff = last_branch_cmmmit['commit_time'] - last_master_commit['commit_time']
+        date_diff = (last_branch_commit_time - last_master_commit_time).seconds
         # Give a min max in date and convert it
         min_date_diff = 0
-        max_date_diff = datetime.datetime.now() - repo['creation_time']
+        max_date_diff = (datetime.datetime.now() - repo_creation_time).seconds
 
         # Convert to actual factor representation
         norm_date_diff = date_diff / (max_date_diff - min_date_diff)
@@ -117,38 +149,54 @@ class GitHubProfiler(AccessPointProfiler):
         # The activity will be dropped down depending on the date diff, to lower the importance of this branch
         merge_dates = []
         for commit in master_branch['commits']:
-            if commit['is_merged'] and commit['merged_branch'] == branch['name']: 
+            if commit['is_merge_commit']:
                 # Found a merged commit from the branch, save the merge date
-                merge_dates.append(commit['commit_time'])
+                merge_dates.append(datetime.datetime.strptime(commit['commit_time'], '%d.%m.%Y'))
         # Calculate the date diffs, and average them, this will give us average diff on master merge
         # Which means how active the branch was within the master branch
-        merge_diffs = numpy.linalg.norm(numpy.diff(merge_dates))
+        merge_diffs = numpy.diff(merge_dates)
         merge_median = numpy.median(merge_diffs)
-
         return merge_median * norm_date_diff
+
+    def __calculate_commit_contribution(self, commit, branch, repo):
+        #       - For each commit produce:
+        #           Commit Size in bytes compared to branch size
+        #               If commit size reduces the branch size, this means removal
+        #               Negative score is allowed for now
+        #           Amount of Diffs on commit (May indicate debugging)
+        #           Commit contribution score:
+        #               (Commit Size / Branch Size)*Wc1 + (DiffsLen)*Wc2 
+        commit_size = commit['post_commit']['branch_size'] - commit['pre_commit']['branch_size']
+        branch_size = commit['post_commit']['branch_size']
+        line_diffs = sum([file_change['file_changed_lines_count'] for file_change in commit['file_changes']])
+        overall_lines = sum([file_change['file_line_count'] for file_change in commit['file_changes']])
+        commit_contribution = (commit_size / branch_size) * self.__weights['commit']['sizes'] + (line_diffs / overall_lines) * self.__weights['commit']['changes']
+        return commit_contribution
 
     def __calculate_branch_contribution(self, branch, repo):
         rolling_score = self.__calculate_rolling_branch_score(branch)
-        branch_importance_factor = self.__calculate_branch_importance(repo, branch, repo['master_branch'])
+        branch_importance_factor = self.__calculate_branch_importance(repo, branch, self.__get_master_branch(repo))
         commits_score = 0.0
         for commit in branch['commits']:
-            commits_score = commits_score + self.__calculate_commit_contribution(commit)
+            commits_score = commits_score + self.__calculate_commit_contribution(commit, branch, repo)
         
         # Normalize score between 0 and 1
         commits_score = (1/len(branch['commits'])-0)*(commits_score-len(branch['commits']))+1
+        
+        return rolling_score * self.__weights['branch']['rolling'] + branch_importance_factor * self.__weights['branch']['importance'] + commits_score * self.__weights['branch']['commits']
 
     def __calculate_repo_contribution(self, repo):
         administrative_score = self.__calculate_repo_administrative_contribution(repo)
-        is_forked = repo['fork']
+        is_forked = repo['is_forked']
         branches_score = 0.0
         for branch in repo['branches']:
-            branches_score = branches_score + self.__calculate_branch_score(branch, re[p])
+            branches_score = branches_score + self.__calculate_branch_contribution(branch, repo)
 
         # Normalize score between 0 and 1
         branches_score = (1/len(repo['branches'])-0)*(branches_score-len(repo['branches']))+1
 
         # Final repo score
-        repo_score = (administrative_score*self.__weights['administrative'] + branches_score*self.__weights['branches']) * (max(self.__weights['fork'], 1-is_forked))
+        repo_score = (administrative_score*self.__weights['repo']['administirive'] + branches_score*self.__weights['repo']['branches']) * (max(self.__weights['repo']['fork'], 1-is_forked))
 
         return repo_score
 
@@ -180,11 +228,12 @@ class GitHubProfiler(AccessPointProfiler):
         # Contribution score => Normalized sum of all repo scores
         
         # Go over every repo and calculate repo score
-        contribution_score = 0.0
+        contribution_scores = []
         for repo in self.__user_repos_metadata:
-            contribution_score = contribution_score + self.__calculate_repo_contribution(repo)
-
-        # TODO - Normalize
+            contribution_scores.append(self.__calculate_repo_contribution(repo))
+        
+        contribution_score = numpy.linalg.norm(contribution_scores)
+        print(contribution_score)
 
     def __calculate_user_scores(self):
         # Calculate contribution score to own repos
@@ -197,117 +246,13 @@ class GitHubProfiler(AccessPointProfiler):
         # Read all the information 
         self.__read_user_workspace()
 
+        # pprint.pprint(self.__user_metadata)
+        # pprint.pprint(self.__user_repos_metadata)
+
         # Perform simple starting scoring before moving into the repos themselves
         self.__calculate_user_scores()
 
 if __name__ == '__main__':
-    fake_ap_userdata = 
-    {
-        'user': 'ofir iluz',
-        'email': 'ofir@ofir.ofir',
-        'creation_time': '16.8.2015',
-        'repos': 
-        [
-            {
-                'repo_name': 'a',
-                'is_downloaded', True
-            },
-            {
-                'repo_name': 'b',
-                'is_downloaded': True
-            }
-        ]
-    }
-
-    fake_ap_repos = 
-    [
-        {
-            'name': 'a',
-            'is_forked': False,
-            'creation_time': '24.9.2016',
-            'pullreqs': [],
-            'issues': [],
-            'branches': 
-            [
-                {
-                    'name': 'master',
-                    'creator': 'ofir iluz',
-                    'creation_time': '24.9.2016',
-                    'commits':
-                    [
-                        {
-                            'author': 'ofir iluz',
-                            'commit_time': '25.9.2016',
-                            'commit': 'init commit',
-                            'is_merge_commit': False,
-                            'merged_branch': '',
-                            'pre_commit':
-                            {
-                                'file_amount': 0
-                            },
-                            'post_commit':
-                            {
-                                'file_amount': 3
-                            },
-                            'file_changes':
-                            [
-                                {
-                                    'file_name': 'gun.cpp',
-                                    'file_line_count': 125,
-                                    'file_changed_lines_count': 125
-                                },
-                                {
-                                    'file_name': 'gun.hpp',
-                                    'file_line_count': 51,
-                                    'file_changed_lines_count': 51
-                                },
-                                {
-                                    'file_name': 'main.cpp',
-                                    'file_line_count': 55,
-                                    'file_changed_lines_count': 55
-                                }
-                            ]
-                        },
-                        {
-                            'author': 'ofir iluz',
-                            'commit_time': '26.9.2016',
-                            'commit': 'Added ak47 gun type',
-                            'is_merge_commit': False,
-                            'merged_branch': '',
-                            'pre_commit':
-                            {
-                                'file_amount': 3
-                            },
-                            'post_commit':
-                            {
-                                'file_amount': 5
-                            },
-                            'changes':
-                            [
-                                {
-                                    'file_name': 'ak47.cpp',
-                                    'file_line_count': 32,
-                                    'file_changed_lines_count': 32
-                                },
-                                {
-                                    'file_name': 'ak47.hpp',
-                                    'file_line_count': 24,
-                                    'file_changed_lines_count': 24
-                                },
-                                {
-                                    'file_name': 'gun.cpp',
-                                    'file_line_count': 142,
-                                    'file_changed_lines_count': 31
-                                }
-                                {
-                                    'file_name': 'main.cpp',
-                                    'file_line_count': 64,
-                                    'file_changed_lines_count': 15
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
+    ofir_meta_file = 'D:\\Octopus\\octopus\\profiler\\ap_profilers\\github\\ofir_iluz'
+    git_profiler = GitHubProfiler(ofir_meta_file)
+    git_profiler.do_profiling()
